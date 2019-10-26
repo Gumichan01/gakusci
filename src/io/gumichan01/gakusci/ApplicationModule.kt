e4e7cba768e5526e3572adcb47a327ce5b8b4fc8
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-10-26 13:21:07 +0200

    Define the API to search for documents
    - Add new application test

--- src/io/gumichan01/gakusci/ApplicationModule.kt
+++ src/io/gumichan01/gakusci/ApplicationModule.kt
@@ -1,12 +1,16 @@
 package io.gumichan01.gakusci
 
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.default
 import io.ktor.http.content.files
 import io.ktor.http.content.static
 import io.ktor.http.content.staticRootFolder
+import io.ktor.response.respond
 import io.ktor.routing.Routing
+import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.thymeleaf.Thymeleaf
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
@@ -24,6 +28,7 @@ fun Application.gakusciModule() {
 
     routing {
         staticPage()
+        restApiSearch()
     }
 }
 
@@ -32,5 +37,11 @@ fun Routing.staticPage() {
         staticRootFolder = File("resources/static")
         files("js")
         default("index.html")
+    }
+}
+
+fun Routing.restApiSearch() {
+    get("/api/v1/researches") {
+        call.respond(HttpStatusCode.OK, "")
     }
 }
--- test/io/gumichan01/gakusci/ApplicationTest.kt
+++ test/io/gumichan01/gakusci/ApplicationTest.kt
@@ -4,15 +4,24 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun `test static homepage`() {
         withTestApplication({ gakusciModule() }) {
             handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
+                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search - return OK`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=lorem").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
             }
         }
     }
