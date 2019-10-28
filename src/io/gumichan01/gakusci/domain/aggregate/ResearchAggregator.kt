b308728c55ad9758610d022b3fc4a4c85dbd271d
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-10-28 16:38:11 +0100

    Improve the aggregation of results
    The previous aggregation algorithm has a big problem.
    It actively waits for the services to finish its job, so it can retrieve data
    (each service is handled by a dedicated coroutine).
    
    This active waiting has consequences on the CPU usage
    (on my computer, 50% of my CPU [2 cores] are used just for that).
    
    This commit fixes that by changing the model of aggregation.
    
    > Old model
    
    In the old model, the aggregator launches services in parallel (1 service = 1 coroutine)
    those services have the role of producers, so each associated coroutine provides a channel to dans data in.
    The agregator has the role of the consumer and has access to those channels.
    
    The consumer checks for each channel if it contains data in order to start retrieving data.
    This consumer will stop the aggregation when every services finished their job,
    i.e. if every channels are closed and has no more data to retrieve.
    
    In the first implementation, the consumer starts retrieving data when each service is terminated,
    and not when the data it sends is available.
    This unexpected behaviour is sequential!
    
    The second implementation is not sequential and start getting data when they are
    available, even if those services are not terminated. However, the aggregator actively waits.
    
    > New model
    
    This model is still using the producer-consumer pattern, but instead of having
    one coroutine that send data to a specific channel. It send data to a channel
    provided by the aggregator. This channel is shared between those producers.
    Those coroutine
    
    The main difference/advantage of this model is that the aggregator can **passively** wait,
    so it can get data when they are available.
    However, there is a potential retention on the producer side.
    It's acceptable!

--- src/io/gumichan01/gakusci/domain/aggregate/ResearchAggregator.kt
+++ src/io/gumichan01/gakusci/domain/aggregate/ResearchAggregator.kt
@@ -2,41 +2,37 @@ package io.gumichan01.gakusci.domain.aggregate
 
 import io.gumichan01.gakusci.domain.model.ResultEntry
 import io.gumichan01.gakusci.domain.service.IService
+import kotlinx.atomicfu.AtomicInt
+import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.receiveOrNull
+import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class ResearchAggregator(private val services: Set<IService>) {
 
     suspend fun search(query: String): List<ResultEntry> {
-        return consumeResults(processRequest(query))
-    }
-
-    private suspend fun consumeResults(channels: List<ReceiveChannel<ResultEntry>>): MutableList<ResultEntry> {
-        var tmpChannels = channels
-        val results: MutableList<ResultEntry> = mutableListOf()
-        while (tmpChannels.isNotEmpty()) {
-            tmpChannels.forEach { channel ->
-                if (!channel.isEmpty) {
-                    val resultEntry: ResultEntry? = channel.receiveOrNull()
-                    resultEntry?.let { entry -> results += entry }
+        val runningCoroutinesCounter: AtomicInt = atomic(services.size)
+        val channel = Channel<ResultEntry>(capacity = 64)
+        services.map { service ->
+            CoroutineScope(Dispatchers.Default).launch {
+                service.search(query).forEach { result -> channel.send(result) }
+                val coroutinesCounter = runningCoroutinesCounter.decrementAndGet()
+                if (coroutinesCounter == 0) {
+                    channel.close()
                 }
             }
-            tmpChannels = tmpChannels.filter { channel -> !channel.isClosedForReceive }
+        }
+
+        val results: MutableList<ResultEntry> = mutableListOf()
+        while (!channel.isClosedForReceive) {
+            val resultEntry: ResultEntry? = channel.receiveOrNull()
+            resultEntry?.let { entry -> results += entry }
         }
         return results
-    }
-
-    private fun processRequest(query: String): List<ReceiveChannel<ResultEntry>> {
-        return services.map { service ->
-            CoroutineScope(Dispatchers.Default).produce(capacity = 64) {
-                service.search(query).forEach { result -> send(result) }
-                close()
-            }
-        }
+        //return consumeResults(processRequest(query))
     }
 }
