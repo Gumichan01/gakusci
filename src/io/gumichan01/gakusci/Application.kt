4cb2f1b04785b4d9bfdcd6b30563216697f9ff90
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-11-09 13:10:34 +0100

    Use the application file in the embedded server
    I need to get a simple way of configuring the application without
    modifying the source code.

--- src/io/gumichan01/gakusci/Application.kt
+++ src/io/gumichan01/gakusci/Application.kt
@@ -1,16 +1,14 @@
 package io.gumichan01.gakusci
 
-import io.ktor.application.Application
+import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.jetty.Jetty
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
-fun main() {
+fun main(args: Array<String>) {
     embeddedServer(
         Jetty,
-        port = 8080,
-        watchPaths = listOf("gakusci"),
-        module = Application::gakusciModule
+        commandLineEnvironment(args)
     ).start(wait = true)
 }
