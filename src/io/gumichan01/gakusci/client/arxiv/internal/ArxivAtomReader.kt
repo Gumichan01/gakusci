bbd47f66f1e4d59dd7b194df39e8c0c5a54c16ad
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-12-06 20:30:22 +0100

    Optimize the use of the Atom reader
    > Each call from the created RssReader will make an HTTP request and return the expected syndication feed.
    
    Source: https://github.com/ouattararomuald/syndication#usage

--- src/io/gumichan01/gakusci/client/arxiv/internal/ArxivAtomReader.kt
+++ src/io/gumichan01/gakusci/client/arxiv/internal/ArxivAtomReader.kt
@@ -2,15 +2,9 @@ package io.gumichan01.gakusci.client.arxiv.internal
 
 import com.ouattararomuald.syndication.Atom
 import io.gumichan01.gakusci.client.arxiv.internal.model.ArxivFeed
-import io.gumichan01.gakusci.client.arxiv.internal.model.OpenSearchFeed
 
-// TODO 2 - Proper implementation
 // TODO 3 - integrate it into the arxiv client
 interface ArxivAtomReader {
-
     @Atom(returnClass = ArxivFeed::class)
-    fun read(): ArxivFeed
-
-    @Atom(returnClass = OpenSearchFeed::class)
-    fun readOpenSearch(): OpenSearchFeed
+    fun readAtom(): ArxivFeed
 }
--- src/io/gumichan01/gakusci/client/arxiv/internal/model/ArxivFeed.kt
+++ src/io/gumichan01/gakusci/client/arxiv/internal/model/ArxivFeed.kt
@@ -1,10 +1,23 @@
 package io.gumichan01.gakusci.client.arxiv.internal.model
 
+import org.simpleframework.xml.Element
 import org.simpleframework.xml.ElementList
 import org.simpleframework.xml.Root
 
 @Root(name = "atom", strict = false)
 data class ArxivFeed(
+    @field:Element(name = "totalResults")
+    @param:Element(name = "totalResults")
+    val totalResults: Int,
+
+    @field:Element(name = "startIndex")
+    @param:Element(name = "startIndex")
+    val startIndex: Int,
+
+    @field:Element(name = "itemsPerPage")
+    @param:Element(name = "itemsPerPage")
+    val itemsPerPage: Int,
+
     @field:ElementList(name = "entry", inline = true)
     @param:ElementList(name = "entry", inline = true)
     val entries: List<Entry>
