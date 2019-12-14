de2da1a1c3077bdaeb6501c8cf966337dd08e14f
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-12-14 15:16:15 +0100

    Integrate the basic cache system

--- src/io/gumichan01/gakusci/domain/search/SearchAggregator.kt
+++ src/io/gumichan01/gakusci/domain/search/SearchAggregator.kt
@@ -2,6 +2,7 @@ package io.gumichan01.gakusci.domain.search
 
 
 import io.gumichan01.gakusci.domain.model.SearchResponse
+import io.gumichan01.gakusci.domain.search.cache.ResultCache
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 
@@ -10,12 +11,13 @@ import kotlinx.coroutines.FlowPreview
 class SearchAggregator(private val searchLauncher: SearchLauncher) {
 
     private val searchResultConsumer = SearchResultConsumer()
+    private val cache = ResultCache()
 
     // TODO Select n first results
     // TODO Set pagination
     // TODO Set cache system
     suspend fun retrieveResults(query: String): SearchResponse {
-        val (total, entries) = searchResultConsumer.consume(searchLauncher.launch(query))
+        val (total, entries) = cache.get(query, searchResultConsumer.consume(searchLauncher.launch(query)))
         return SearchResponse(total, 0, entries)
     }
 }
