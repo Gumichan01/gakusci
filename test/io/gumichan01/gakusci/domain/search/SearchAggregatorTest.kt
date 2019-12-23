e2d5b45118c33219603e7f294f4edfe2214d5c64
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-12-23 19:42:14 +0100

    Fix aggregator tests

--- test/io/gumichan01/gakusci/domain/search/SearchAggregatorTest.kt
+++ test/io/gumichan01/gakusci/domain/search/SearchAggregatorTest.kt
@@ -1,12 +1,12 @@
 package io.gumichan01.gakusci.domain.search
 
 import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.gumichan01.gakusci.domain.model.ResultEntry
 import io.gumichan01.gakusci.domain.model.SearchResponse
 import io.gumichan01.gakusci.domain.model.ServiceResponse
 import io.gumichan01.gakusci.domain.utils.SearchType
-import io.gumichan01.gakusci.domain.utils.getOrUpdateCache
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -23,33 +23,33 @@ class SearchAggregatorTest {
             runBlocking { send(ServiceResponse(1, listOf(ResultEntry("lorem", "ipsum")))); close() }; this
         }
     }
-    private val fakeCache: Cache<Pair<String, Int>, ServiceResponse> = mockk {
-        every { getOrUpdateCache(Pair("lorem", 1)) { ServiceResponse(1, listOf(ResultEntry("lorem", "ipsum"))) } }
-    }
+    private val fakeCache: Cache<Pair<String, Int>, ServiceResponse> = mockk()
+
+    private val testCache: Cache<Pair<String, Int>, ServiceResponse> = Caffeine.newBuilder().maximumSize(10).build()
 
     @Test
     fun `aggregate result entries - return results`() {
-        val aggregator = SearchAggregator(fakeLauncher, fakeCache)
+        val aggregator = SearchAggregator(fakeLauncher, testCache)
         val results: SearchResponse =
-            runBlocking { aggregator.retrieveResults(QueryParam("lorem", SearchType.RESEARCH, rows = 1)) }
+            runBlocking { aggregator.retrieveResults(QueryParam("lorem", SearchType.RESEARCH)) }
         assertThat(results.totalResults).isEqualTo(1)
         assertThat(results.entries).containsAnyOf(ResultEntry("lorem", "ipsum"))
     }
 
     @Test
     fun `build research aggregator linked to no service`() {
-        assertThat(SearchAggregator.Builder().build()).isInstanceOf(SearchAggregator::class.java)
+        assertThat(SearchAggregator.Builder().withCache(fakeCache).build()).isInstanceOf(SearchAggregator::class.java)
     }
 
     @Test
     fun `build research aggregator with research services`() {
-        assertThat(SearchAggregator.Builder().withResearchServices().build()).isInstanceOf(SearchAggregator::class.java)
+        assertThat(SearchAggregator.Builder().withResearchServices().withCache(fakeCache).build())
+            .isInstanceOf(SearchAggregator::class.java)
     }
 
     @Test
     fun `build research aggregator with research services and a cache system`() {
-        assertThat(SearchAggregator.Builder().withResearchServices().withCache(fakeCache).build()).isInstanceOf(
-            SearchAggregator::class.java
-        )
+        assertThat(SearchAggregator.Builder().withResearchServices().withCache(fakeCache).build())
+            .isInstanceOf(SearchAggregator::class.java)
     }
 }
