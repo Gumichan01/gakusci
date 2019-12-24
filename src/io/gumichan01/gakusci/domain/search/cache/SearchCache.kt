4bf14a563d88cc2a619e7889625235a8dc38bc66
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-12-24 13:56:44 +0100

    Integrate extension functions from Cache to SearchCache
    Some extension functions were used in Cache (provided by Caffeine).
    This commit move them to SearchCache with is a wrapper of thiscache.
    SearchCache uses delegation in order to use internally their
    Caffeine cache without using inheritance.

--- src/io/gumichan01/gakusci/domain/search/cache/SearchCache.kt
+++ src/io/gumichan01/gakusci/domain/search/cache/SearchCache.kt
@@ -6,4 +6,29 @@ import io.gumichan01.gakusci.domain.model.ServiceResponse
 class SearchCache(cache: Cache<Pair<String, Int>, ServiceResponse>) :
     Cache<Pair<String, Int>, ServiceResponse> by cache {
 
+    fun getOrUpdateCache(key: Pair<String, Int>, f: () -> ServiceResponse): ServiceResponse {
+        val keyByQueryName: Pair<String, Int>? = getKeyOrNullByQueryName(key.query())
+        return if (keyByQueryName != null) {
+            if (keyByQueryName.rows() < key.rows()) {
+                invalidate(keyByQueryName)
+                getCachedValue(key, f)
+            } else {
+                getCachedValue(keyByQueryName, f)
+            }
+        } else getCachedValue(key, f)
+    }
+
+    private fun getCachedValue(key: Pair<String, Int>, f: () -> ServiceResponse): ServiceResponse {
+        return get(key) { f() } ?: throw IllegalStateException("Illegal state of cache: ${asMap()}")
+    }
+
+    private fun getKeyOrNullByQueryName(query: String): Pair<String, Int>? {
+        return asMap().keys.asSequence()
+            .filter { key -> key != null }
+            .filter { key -> key.query() == query }.take(1)
+            .firstOrNull()
+    }
+
+    private fun Pair<String, Int>.query() = first
+    private fun Pair<String, Int>.rows() = second
 }
