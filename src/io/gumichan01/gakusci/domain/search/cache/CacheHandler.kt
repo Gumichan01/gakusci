db4e2d1a4fe29a7259a797485b6625767c2c5de5
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-12-31 13:23:14 +0100

    Modify the cached value validity [cache]
    Some cached values must be valid during a fixed durration since
    the last creation or replacement.

--- src/io/gumichan01/gakusci/domain/search/cache/CacheHandler.kt
+++ src/io/gumichan01/gakusci/domain/search/cache/CacheHandler.kt
@@ -3,10 +3,13 @@ package io.gumichan01.gakusci.domain.search.cache
 import com.github.benmanes.caffeine.cache.Caffeine
 import io.gumichan01.gakusci.domain.model.ServiceResponse
 import io.gumichan01.gakusci.domain.utils.SearchType
+import java.util.concurrent.TimeUnit
 
 class CacheHandler {
 
-    private val builder: Caffeine<Any, Any> by lazy { Caffeine.newBuilder().maximumSize(10L) }
+    private val builder: Caffeine<Any, Any> by lazy {
+        Caffeine.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).maximumSize(8L)
+    }
     private val researchCache: SearchCache by lazy { SearchCache(builder.build<Pair<String, Int>, ServiceResponse>()) }
     private val bookCache: SearchCache by lazy { SearchCache(builder.build<Pair<String, Int>, ServiceResponse>()) }
 
