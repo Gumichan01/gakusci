e76b0bf4bb7f9ce9f37db003ca3afb87d89dc222
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-02 18:47:47 +0100

    Set the cached value validation time to 10 minutes [cache]

--- src/io/gumichan01/gakusci/domain/search/cache/CacheHandler.kt
+++ src/io/gumichan01/gakusci/domain/search/cache/CacheHandler.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 class CacheHandler {
 
     private val builder: Caffeine<Any, Any> by lazy {
-        Caffeine.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).maximumSize(8L)
+        Caffeine.newBuilder().expireAfterWrite(600, TimeUnit.SECONDS).maximumSize(8L)
     }
     private val researchCache: SearchCache by lazy { SearchCache(builder.build<Pair<String, Int>, ServiceResponse>()) }
     private val bookCache: SearchCache by lazy { SearchCache(builder.build<Pair<String, Int>, ServiceResponse>()) }
