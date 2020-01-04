aeec6812ceddda3ea8ffa01d8b94bb27e85fa60b
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-04 14:31:16 +0100

    Make the REST API controller more consistent [API]

--- src/io/gumichan01/gakusci/controller/utils/ControllerUtils.kt
+++ src/io/gumichan01/gakusci/controller/utils/ControllerUtils.kt
@@ -22,27 +22,51 @@ fun retrieveWebParam(queryParameters: Parameters): Pair<QueryParam?, String> {
     } ?: Pair(null, "Bad request: no query parameter 'q' provided")
 }
 
-// TODO make the API consistent
 fun retrieveApiParam(queryParameters: Parameters, pathParameters: Parameters): Pair<QueryParam?, String> {
     return queryParameters["q"]?.let { query ->
-        val start = queryParameters["start"]?.toInt() ?: 0
-        val rows = queryParameters["max_results"]?.toInt() ?: 10
-        val numPerPage: Int? = queryParameters["num_per_page"]?.toInt()
-        val searchType: SearchType? = getApiSearchTypeFrom(pathParameters)
+        getApiSearchTypeFrom(pathParameters)?.let { searchType ->
+            val start: Int? = queryParameters["start"]?.toInt()
+            val rows: Int? = queryParameters["max_results"]?.toInt()
+            val numPerPage: Int? = queryParameters["num_per_page"]?.toInt()
 
-        when {
-            query.isBlank() -> Pair(null, "Bad request: query parameter 'q' is blank")
-            start < 0 -> Pair(null, "Bad request: negative start value: $start")
-            rows < 0 -> Pair(null, "Bad request: negative rows value: $rows")
-            start > rows -> Pair(null, "Bad request: start is greater than max_results")
-            searchType == null -> Pair(null, "Bad request: no query parameter 'stype' provided")
-            numPerPage != null && numPerPage > rows -> {
-                Pair(null, "Bad request: cannot get more entries per page than max_results")
+            when {
+                rows != null && rows < 0 -> Pair(null, "Bad request: negative max_results value: $rows")
+                start != null -> {
+                    RestApiQueryParameters(query, searchType, rows, start, numPerPage).run {
+                        retrieveAndCheckRestApiQueryParameters(this)
+                    }
+                }
+                numPerPage != null -> Pair(null, "Bad request: cannot set 'num_per_page' with no 'start' value")
+                else -> {
+                    val maxResults = rows ?: 10
+                    Pair(QueryParam(query, searchType, maxResults, 0, numPerPage), "")
+                }
             }
-            else -> Pair(QueryParam(query, searchType, rows, start, numPerPage), "")
-        }
+        } ?: Pair(null, "Bad request: no query parameter 'stype' provided")
     } ?: Pair(null, "Bad request: no query parameter 'q' provided")
 }
+
+private fun retrieveAndCheckRestApiQueryParameters(queryParameters: RestApiQueryParameters): Pair<QueryParam?, String> {
+    return queryParameters.run {
+        when {
+            start < 0 -> Pair(null, "Bad request: negative 'start' value: $start")
+            numPerPage == null -> Pair(null, "Bad request: cannot set 'start' with no 'num_per_page'")
+            numPerPage < 0 -> Pair(null, "Bad request: negative 'num_per_page' value: $start")
+            rows == null -> Pair(null, "Bad request: cannot set 'start' with no 'max_results'")
+            start > rows -> Pair(null, "Bad request: 'start' is greater than 'max_results'")
+            numPerPage > rows -> Pair(null, "Bad request: 'num_per_page' is greater than 'max_results'")
+            else -> Pair(QueryParam(query, searchType, rows, start, numPerPage), "")
+        }
+    }
+}
+
+private data class RestApiQueryParameters(
+    val query: String,
+    val searchType: SearchType,
+    val rows: Int?,
+    val start: Int,
+    val numPerPage: Int?
+)
 
 private fun getSearchTypeFrom(parameters: Parameters): SearchType? {
     return parameters["stype"]?.let {
--- test/io/gumichan01/gakusci/ApplicationTest.kt
+++ test/io/gumichan01/gakusci/ApplicationTest.kt
@@ -22,25 +22,6 @@ class ApplicationTest {
     }
 
     @Test
-    fun `test REST Web-service API v1 search, normal case - return OK and non empty content`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=lorem").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
-                assertThat(response.content).isNotBlank()
-            }
-        }
-    }
-
-    @Test
-    fun `test REST Web-service API v1 search, no query - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
-        }
-    }
-
-    @Test
     fun `test web application, normal case - return OK and HTML content`() {
         withTestApplication({ gakusciModule() }) {
             handleRequest(HttpMethod.Get, "/search/?q=lorem&stype=research").apply {
@@ -66,42 +47,6 @@ class ApplicationTest {
     fun `test web application search, query with negative start value - return Bad request`() {
         withTestApplication({ gakusciModule() }) {
             handleRequest(HttpMethod.Get, "/search/?q=lorem&stype=research&start=-10").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
-        }
-    }
-
-    @Test
-    fun `test REST Web-service API v1 search, query with start greater than max_results - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&max_results=10&start=100").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
-        }
-    }
-
-    @Test
-    fun `test REST Web-service API v1 search, query with negative start value - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&start=-10").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
-        }
-    }
-
-    @Test
-    fun `test REST Web-service API v1 search, negative max_results - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=b&max_results=-1").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
-        }
-    }
-
-    @Test
-    fun `test REST Web-service API v1 search, query with numPerPage greater than max_results - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=b&max_results=10&start=1&num_per_page=100000").apply {
                 assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
@@ -136,6 +81,115 @@ class ApplicationTest {
                 with(response) {
                     assertThat(status()).isEqualTo(HttpStatusCode.BadRequest)
                 }
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, normal case - return OK and non empty content`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=lorem").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+                assertThat(response.content).isNotBlank()
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, no query - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, query with start but no num_per_page - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&start=0").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, query with num_per_page but no start - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&num_per_page=2").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, query with start but no max_results - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&start=0").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, query with pagination (start, num_per_page, max_results) - return ok`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&start=0&num_per_page=4&max_results=16").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, query with start greater than max_results - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&max_results=10&start=100&num_per_page=2").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, query with negative start value - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a&start=-10&max_results=16&num_per_page=2").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, pagination but with negative max_results - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=b&max_results=-1&start=0&num_per_page=2").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, negative max_results without pagination - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=b&max_results=-1").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, negative num_per_page - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=b&max_results=10&start=0&num_per_page=-1").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
+            }
+        }
+    }
+
+    @Test
+    fun `test REST Web-service API v1 search, query with numPerPage greater than max_results - return Bad request`() {
+        withTestApplication({ gakusciModule() }) {
+            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=b&max_results=10&start=1&num_per_page=100000").apply {
+                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
     }
