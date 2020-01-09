884d2e938c99cbe6f2cf056b25bda6c11e7b2509
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-09 19:11:36 +0100

    Set the client properly (simple version with ISBN)

--- src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
+++ src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
@@ -1,6 +1,8 @@
 package io.gumichan01.gakusci.client.openlib
 
 import io.gumichan01.gakusci.client.IClient
+import io.gumichan01.gakusci.client.utils.isValidISBN
+import io.gumichan01.gakusci.client.utils.normalizeBookNumber
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
@@ -10,8 +12,16 @@ class OpenLibraryBookClient: IClient<String> {
     private val openLibrarySearchUrl = "http://openlibrary.org/api/books?bibkeys=%s&format=json"
 
     override suspend fun retrieveResults(queryParam: QueryParam): String? {
-        // TODO OpenLibraryBookClient 1. Check valid ISBN/LCCN/OCLC number
+        // TODO OpenLibraryBookClient 1. Check valid LCCN/OCLC number
         // TODO OpenLibraryBookClient 2. Deserialize from JSON to OpenLibraryBookResponse
-        return HttpClient(Apache).use { it.get(openLibrarySearchUrl.format(queryParam.query)) }
+        return HttpClient(Apache).use { it.get(openLibrarySearchUrl.format(formatBookNumber(queryParam.query))) }
+    }
+
+    private fun formatBookNumber(bookNumber: String): String {
+        return StringBuilder().run {
+            if (isValidISBN(bookNumber)) {
+                append("ISBN:${normalizeBookNumber(bookNumber)}")
+            } else this
+        }.toString()
     }
 }
--- test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
+++ test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
@@ -5,14 +5,25 @@ import io.gumichan01.gakusci.domain.model.QueryParam
 import io.gumichan01.gakusci.domain.utils.SearchType
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import java.lang.reflect.Method
 import kotlin.test.Test
 
 internal class OpenLibraryBookClientTest {
 
-    @Test
+//    @Test
     fun `Call the Open Library web-service - get book by ISBN`() {
         val client: IClient<String> = OpenLibraryBookClient()
         val response: String? = runBlocking { client.retrieveResults(QueryParam("ISBN:1421500574", SearchType.BOOKS)) }
         assertThat(response).isNotNull()
     }
+
+//    @Test // Note: formatBookNumber()  is private, so you need to make it public in order to test is
+//    fun `format valid ISBN - get formatted text`() {
+//        assertThat(OpenLibraryBookClient().formatBookNumber("978-1-4215-0057-7")).isEqualTo("ISBN:9781421500577")
+//    }
+//
+//    @Test
+//    fun `format invalid ISBN - get empty text`() {
+//        assertThat(OpenLibraryBookClient().formatBookNumber("978-1-4215-0057-0")).isBlank()
+//    }
 }
