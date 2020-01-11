384253cf7e28fdafaf0b392c578623b977b00786
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-11 14:07:51 +0100

    Add new tests in the Open Library Book client

--- test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
+++ test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
@@ -9,13 +9,26 @@ import kotlin.test.Test
 
 internal class OpenLibraryBookClientTest {
 
-    //    @Test
+//    @Test
     fun `Call the Open Library web-service - get book by ISBN`() {
         val client: IClient<String> = OpenLibraryBookClient()
         val response: String? = runBlocking { client.retrieveResults(QueryParam("ISBN:1421500574", SearchType.BOOKS)) }
         assertThat(response).isNotNull()
     }
 
+//    @Test
+    fun `Call the Open Library web-service - get book by LCCN`() {
+        val client: IClient<String> = OpenLibraryBookClient()
+        val response: String? = runBlocking { client.retrieveResults(QueryParam("LCCN:n78890351", SearchType.BOOKS)) }
+        assertThat(response).isNotNull()
+    }
+
+//    @Test
+    fun `Call the Open Library web-service - get book by OCLC number`() {
+        val client: IClient<String> = OpenLibraryBookClient()
+        val response: String? = runBlocking { client.retrieveResults(QueryParam("OCLC:18936737", SearchType.BOOKS)) }
+        assertThat(response).isNotNull()
+    }
 //    @Test // Note: formatBookNumber()  is private, so you need to make it public in order to test is
 //    fun `format valid ISBN - get formatted text`() {
 //        assertThat(OpenLibraryBookClient().formatBookNumber("978-1-4215-0057-7")).isEqualTo("ISBN:9781421500577")
