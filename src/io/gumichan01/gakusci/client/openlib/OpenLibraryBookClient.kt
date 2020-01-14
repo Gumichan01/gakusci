80b59321939a8db7bdcb5c596b83e26a9b1d5bcc
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-14 12:05:59 +0100

    Use the book number generator instead of making the client do it itself

--- src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
+++ src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
@@ -3,7 +3,8 @@ package io.gumichan01.gakusci.client.openlib
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.gumichan01.gakusci.client.IClient
-import io.gumichan01.gakusci.client.utils.*
+import io.gumichan01.gakusci.client.utils.BookNumber
+import io.gumichan01.gakusci.client.utils.generateBookNumberFromText
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
@@ -13,25 +14,12 @@ class OpenLibraryBookClient : IClient<List<OpenLibraryBookResponse>> {
     private val openLibrarySearchUrl = "http://openlibrary.org/api/books?bibkeys=%s&format=json"
 
     override suspend fun retrieveResults(queryParam: QueryParam): List<OpenLibraryBookResponse>? {
-        // TODO OpenLibraryBookClient 2. Deserialize from JSON to OpenLibraryBookResponse
-        val bookNumber: String = getFormattedBookNumber(queryParam.query)
-        return if (!bookNumber.isBlank()) {
+        val bookNumber: BookNumber? = generateBookNumberFromText(queryParam.query)
+        return if (bookNumber != null) {
             HttpClient(Apache).use { it.get<String>(openLibrarySearchUrl.format(bookNumber)) }.fromJson()
         } else {
             null
         }
-    }
-
-    private fun getFormattedBookNumber(bookNumber: String): String {
-        return StringBuilder().run {
-            when {
-                isValidISBN(bookNumber) -> append("ISBN:${normalizeIsbn(bookNumber)}")
-                // A valid OCLC number a valid LCCN
-                isValidOCLC(bookNumber) -> append("OCLC:$bookNumber,LCCN:${normalizeLccn(bookNumber)}")
-                isValidLCCN(normalizeLccn(bookNumber)) -> append("LCCN:${normalizeLccn(bookNumber)}")
-                else -> this
-            }
-        }.toString()
     }
 
     private fun String.fromJson(): List<OpenLibraryBookResponse> {
--- test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
+++ test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
@@ -9,60 +9,35 @@ import kotlin.test.Test
 
 internal class OpenLibraryBookClientTest {
 
-//    @Test
+    @Test
     fun `Call the Open Library web-service - get book by ISBN`() {
         val client: IClient<List<OpenLibraryBookResponse>> = OpenLibraryBookClient()
-        val response: List<OpenLibraryBookResponse>? = runBlocking { client.retrieveResults(QueryParam("1421500574", SearchType.BOOKS)) }
+        val response: List<OpenLibraryBookResponse>? =
+            runBlocking { client.retrieveResults(QueryParam("1421500574", SearchType.BOOKS)) }
         assertThat(response).isNotNull
     }
 
-//    @Test
+    @Test
     fun `Call the Open Library web-service - get book by LCCN`() {
         val client: IClient<List<OpenLibraryBookResponse>> = OpenLibraryBookClient()
-        val response: List<OpenLibraryBookResponse>? = runBlocking { client.retrieveResults(QueryParam("62019420", SearchType.BOOKS)) }
+        val response: List<OpenLibraryBookResponse>? =
+            runBlocking { client.retrieveResults(QueryParam("62019420", SearchType.BOOKS)) }
         assertThat(response).isNotNull
     }
 
-//    @Test
+    @Test
     fun `Call the Open Library web-service - get book by OCLC number`() {
         val client: IClient<List<OpenLibraryBookResponse>> = OpenLibraryBookClient()
-        val response: List<OpenLibraryBookResponse>? = runBlocking { client.retrieveResults(QueryParam("18936737", SearchType.BOOKS)) }
+        val response: List<OpenLibraryBookResponse>? =
+            runBlocking { client.retrieveResults(QueryParam("18936737", SearchType.BOOKS)) }
         assertThat(response).isNotNull
     }
 
     @Test
     fun `Don't call the Open Library web-service - invalid ISBN or LCCN or OCLC`() {
         val client: IClient<List<OpenLibraryBookResponse>> = OpenLibraryBookClient()
-        val response: List<OpenLibraryBookResponse>? = runBlocking { client.retrieveResults(QueryParam("1$9367e7", SearchType.BOOKS)) }
+        val response: List<OpenLibraryBookResponse>? =
+            runBlocking { client.retrieveResults(QueryParam("1$9367e7", SearchType.BOOKS)) }
         assertThat(response).isNull()
     }
-//    @Test // Note: formatBookNumber()  is private, so you need to make it public in order to test is
-//    fun `format valid ISBN - get formatted text`() {
-//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("978-1-4215-0057-7")).isEqualTo("ISBN:9781421500577")
-//    }
-//
-//    @Test
-//    fun `format invalid ISBN - get empty text`() {
-//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("978-1-4215-0057-0")).isBlank()
-//    }
-//
-//    @Test
-//    fun `format valid LCCN - get formatted text`() {
-//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("n78890351")).isEqualTo("LCCN:n78890351")
-//    }
-//
-//    @Test
-//    fun `format invalid LCCN - get empty text`() {
-//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("478890351")).isBlank()
-//    }
-//
-//    @Test
-//    fun `format valid OCLC - get formatted text`() {
-//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("18936737")).isEqualTo("OCLC:18936737")
-//    }
-//
-//    @Test
-//    fun `format invalid OCLC - get empty text`() {
-//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("1$9367e7")).isBlank()
-//    }
 }
