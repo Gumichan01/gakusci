fcd518036e2997c65fae32b0a384b5ee1c4d59ae
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-14 15:54:06 +0100

    Surround the client call with a try-catch

--- src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
+++ src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
@@ -3,21 +3,33 @@ package io.gumichan01.gakusci.client.openlib
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.gumichan01.gakusci.client.IClient
+import io.gumichan01.gakusci.client.hal.HalClient
 import io.gumichan01.gakusci.client.utils.BookNumber
 import io.gumichan01.gakusci.client.utils.generateBookNumberFromText
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.request.get
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class OpenLibraryBookClient : IClient<OpenLibraryBookResponse> {
+
+    private val logger: Logger = LoggerFactory.getLogger(OpenLibraryBookClient::class.java)
     private val openLibrarySearchUrl = "http://openlibrary.org/api/books?bibkeys=%s&format=json"
 
     override suspend fun retrieveResults(queryParam: QueryParam): OpenLibraryBookResponse? {
         val bookNumber: BookNumber? = generateBookNumberFromText(queryParam.query)
         return if (bookNumber != null) {
-            // TODO try-catch in the client
-            HttpClient(Apache).use { it.get<String>(openLibrarySearchUrl.format(bookNumber.format())) }.fromJson()
+            try {
+                HttpClient(Apache).use { it.get<String>(openLibrarySearchUrl.format(bookNumber.format())) }.fromJson()
+            } catch (e: Exception) {
+                logger.trace(e.message)
+                if (logger.isTraceEnabled) {
+                    e.printStackTrace()
+                }
+                null
+            }
         } else {
             null
         }
