ae584c054bd86820b971dc65e4a96bb86a75b16f
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-14 11:14:56 +0100

    Don't call the book web-service if the book number text is blank

--- src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
+++ src/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClient.kt
@@ -14,12 +14,15 @@ class OpenLibraryBookClient : IClient<List<OpenLibraryBookResponse>> {
 
     override suspend fun retrieveResults(queryParam: QueryParam): List<OpenLibraryBookResponse>? {
         // TODO OpenLibraryBookClient 2. Deserialize from JSON to OpenLibraryBookResponse
-        // TODO don't call the external servie if the formmated book number is blank
-        return HttpClient(Apache).use { it.get<String>(openLibrarySearchUrl.format(formatBookNumber(queryParam.query))) }
-            .fromJson()
+        val bookNumber: String = getFormattedBookNumber(queryParam.query)
+        return if (!bookNumber.isBlank()) {
+            HttpClient(Apache).use { it.get<String>(openLibrarySearchUrl.format(bookNumber)) }.fromJson()
+        } else {
+            null
+        }
     }
 
-    private fun formatBookNumber(bookNumber: String): String {
+    private fun getFormattedBookNumber(bookNumber: String): String {
         return StringBuilder().run {
             when {
                 isValidISBN(bookNumber) -> append("ISBN:${normalizeIsbn(bookNumber)}")
--- test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
+++ test/io/gumichan01/gakusci/client/openlib/OpenLibraryBookClientTest.kt
@@ -29,33 +29,40 @@ internal class OpenLibraryBookClientTest {
         val response: List<OpenLibraryBookResponse>? = runBlocking { client.retrieveResults(QueryParam("18936737", SearchType.BOOKS)) }
         assertThat(response).isNotNull
     }
+
+    @Test
+    fun `Don't call the Open Library web-service - invalid ISBN or LCCN or OCLC`() {
+        val client: IClient<List<OpenLibraryBookResponse>> = OpenLibraryBookClient()
+        val response: List<OpenLibraryBookResponse>? = runBlocking { client.retrieveResults(QueryParam("1$9367e7", SearchType.BOOKS)) }
+        assertThat(response).isNull()
+    }
 //    @Test // Note: formatBookNumber()  is private, so you need to make it public in order to test is
 //    fun `format valid ISBN - get formatted text`() {
-//        assertThat(OpenLibraryBookClient().formatBookNumber("978-1-4215-0057-7")).isEqualTo("ISBN:9781421500577")
+//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("978-1-4215-0057-7")).isEqualTo("ISBN:9781421500577")
 //    }
 //
 //    @Test
 //    fun `format invalid ISBN - get empty text`() {
-//        assertThat(OpenLibraryBookClient().formatBookNumber("978-1-4215-0057-0")).isBlank()
+//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("978-1-4215-0057-0")).isBlank()
 //    }
 //
-//    @Test // Note: formatBookNumber()  is private, so you need to make it public in order to test is
+//    @Test
 //    fun `format valid LCCN - get formatted text`() {
-//        assertThat(OpenLibraryBookClient().formatBookNumber("n78890351")).isEqualTo("LCCN:n78890351")
+//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("n78890351")).isEqualTo("LCCN:n78890351")
 //    }
 //
 //    @Test
 //    fun `format invalid LCCN - get empty text`() {
-//        assertThat(OpenLibraryBookClient().formatBookNumber("478890351")).isBlank()
+//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("478890351")).isBlank()
 //    }
 //
 //    @Test
 //    fun `format valid OCLC - get formatted text`() {
-//        assertThat(OpenLibraryBookClient().formatBookNumber("18936737")).isEqualTo("OCLC:18936737")
+//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("18936737")).isEqualTo("OCLC:18936737")
 //    }
 //
 //    @Test
 //    fun `format invalid OCLC - get empty text`() {
-//        assertThat(OpenLibraryBookClient().formatBookNumber("1$9367e7")).isBlank()
+//        assertThat(OpenLibraryBookClient().getFormattedBookNumber("1$9367e7")).isBlank()
 //    }
 }
