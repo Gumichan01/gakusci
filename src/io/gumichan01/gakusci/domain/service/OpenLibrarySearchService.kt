ecfe58506113441c5a5e426fb926e25deed6d6e2
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-01-16 20:25:19 +0100

    Set book entries as returned values

--- src/io/gumichan01/gakusci/domain/service/OpenLibrarySearchService.kt
+++ src/io/gumichan01/gakusci/domain/service/OpenLibrarySearchService.kt
@@ -3,18 +3,17 @@ package io.gumichan01.gakusci.domain.service
 import io.gumichan01.gakusci.client.IClient
 import io.gumichan01.gakusci.client.openlib.OpenLibrarySearchResponse
 import io.gumichan01.gakusci.domain.model.QueryParam
-import io.gumichan01.gakusci.domain.model.entry.SimpleResultEntry
 import io.gumichan01.gakusci.domain.model.ServiceResponse
+import io.gumichan01.gakusci.domain.model.entry.BookEntry
+import io.gumichan01.gakusci.domain.model.entry.IResultEntry
+import io.gumichan01.gakusci.domain.model.entry.SimpleResultEntry
 
 class OpenLibrarySearchService(private val openLibrarySearchClient: IClient<OpenLibrarySearchResponse>) : IService {
     override suspend fun search(queryParam: QueryParam): ServiceResponse? {
         return openLibrarySearchClient.retrieveResults(queryParam)?.let {
             val numFound = it.numFound
-            val entries: List<SimpleResultEntry> = it.docs?.map { d ->
-                SimpleResultEntry(
-                    d.label(),
-                    d.link()
-                )
+            val entries: List<IResultEntry> = it.docs?.map { d ->
+                BookEntry(SimpleResultEntry(d.label(), d.link()), "")
             } ?: emptyList()
             ServiceResponse(numFound, entries)
         }
