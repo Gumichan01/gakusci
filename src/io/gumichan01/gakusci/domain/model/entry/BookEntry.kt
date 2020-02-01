9da077c8a5e490cb22e9e53d0a78fcffc1bb5875
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-02-01 11:56:52 +0100

    Delegate methods of BookEntry

--- src/io/gumichan01/gakusci/domain/model/entry/BookEntry.kt
+++ src/io/gumichan01/gakusci/domain/model/entry/BookEntry.kt
@@ -1,7 +1,3 @@
 package io.gumichan01.gakusci.domain.model.entry
 
-data class BookEntry(val entry: SimpleResultEntry, val thumbnailUrl: String): IResultEntry {
-    override fun label() = entry.label
-    override fun link() = entry.url
-
-}
+data class BookEntry(val entry: SimpleResultEntry, val thumbnailUrl: String): IResultEntry by entry
