ccc89ca2481c08df483246a7b1b2ea3a5a30e3a9
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-02-02 15:57:36 +0100

    Fix bad pagination of the book search API

--- src/io/gumichan01/gakusci/domain/service/OpenLibrarySearchService.kt
+++ src/io/gumichan01/gakusci/domain/service/OpenLibrarySearchService.kt
@@ -11,10 +11,12 @@ import io.gumichan01.gakusci.domain.model.entry.SimpleResultEntry
 class OpenLibrarySearchService(private val openLibrarySearchClient: IClient<OpenLibrarySearchResponse>) : IService {
     override suspend fun search(queryParam: QueryParam): ServiceResponse? {
         return openLibrarySearchClient.retrieveResults(queryParam)?.let {
-            val numFound = it.numFound
-            val entries: List<IResultEntry> = it.docs?.map { doc ->
+            // Since the API is experimental and returns by default at most 100 results per page (even the first page)
+            // The service will take 100 results
+            val numFound = 100
+            val entries: List<IResultEntry> = it.docs?.asSequence()?.map { doc ->
                 BookEntry(SimpleResultEntry(doc.label(), doc.link()), doc.thumbnail())
-            } ?: emptyList()
+            }?.take(numFound)?.toList() ?: emptyList()
             ServiceResponse(numFound, entries)
         }
     }
