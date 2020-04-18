060b051e16f29e7f852ee04bce8c9cedc3fc8850
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-04-18 13:51:35 +0200

    Set ISBN-10 and ISBN-13 validation functions public
    Those functions are defined in an utility file used in any
    service. Since there is two types of ISBNs, it makes sense to let
    the user the possibility to check validation of one of the two types
    if needed.

--- src/io/gumichan01/gakusci/client/utils/BookNumberUtils.kt
+++ src/io/gumichan01/gakusci/client/utils/BookNumberUtils.kt
@@ -43,22 +43,22 @@ fun isValidISBN(bookNumber: String): Boolean {
 }
 
 // Check https://isbn-information.com/the-10-digit-isbn.html
-private fun isValidISBN10(bookNumber: String): Boolean {
+fun isValidISBN10(bookNumber: String): Boolean {
     val isbn10WithoutSeparatorRegex = "\\d{10}\$"
     val isbn10Regex = "\\d{1,5}([- ])\\d{1,7}\\1\\d{1,6}\\1(\\d|X)\$"
     return bookNumber.matches(Regex("$isbn10Regex|$isbn10WithoutSeparatorRegex")) && isValidCheckDigitISBN10(bookNumber)
 }
 
 private fun isValidCheckDigitISBN10(bookNumber: String): Boolean {
-    val simplifiedIsbn13: String = normalizeIsbn(bookNumber)
-    return simplifiedIsbn13.length == 10 &&
-            simplifiedIsbn13.map { c -> c.toInt() }
+    val simplifiedIsbn10: String = normalizeIsbn(bookNumber)
+    return simplifiedIsbn10.length == 10 &&
+            simplifiedIsbn10.map { c -> c.toInt() }
                 .mapIndexed { i, v -> v * (10 - i) }
                 .reduce { acc, sum -> acc + sum } % 11 == 0
 }
 
 // Check https://isbn-information.com/the-13-digit-isbn.html
-private fun isValidISBN13(bookNumber: String): Boolean {
+fun isValidISBN13(bookNumber: String): Boolean {
     val isbn13WithoutSeparatorRegex = "\\d{13}\$"
     val isbn13Regex = "97(?:8|9)([ -])\\d{1,5}\\1\\d{1,7}\\1\\d{1,6}\\1\\d\$"
     return bookNumber.matches(Regex("$isbn13Regex|$isbn13WithoutSeparatorRegex")) && isValidCheckDigitISBN13(bookNumber)
