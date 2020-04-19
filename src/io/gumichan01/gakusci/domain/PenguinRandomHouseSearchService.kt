e42c9cc4176d2a060c85aa380a54699184ebec0b
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-04-19 14:17:12 +0200

    Handle valid request with no result produced

--- src/io/gumichan01/gakusci/domain/PenguinRandomHouseSearchService.kt
+++ src/io/gumichan01/gakusci/domain/PenguinRandomHouseSearchService.kt
@@ -1,14 +1,19 @@
 package io.gumichan01.gakusci.domain
 
+import io.gumichan01.gakusci.client.IClient
+import io.gumichan01.gakusci.client.penguin.PenguinRandomHouseSearchResponse
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.gumichan01.gakusci.domain.model.ServiceResponse
+import io.gumichan01.gakusci.domain.model.entry.BookEntry
 import io.gumichan01.gakusci.domain.service.IService
 
-class PenguinRandomHouseSearchService : IService {
+class PenguinRandomHouseSearchService(private val searchClient: IClient<PenguinRandomHouseSearchResponse>) : IService {
     override suspend fun search(queryParam: QueryParam): ServiceResponse? {
-        return if (queryParam.query == "9780140043204")
-            ServiceResponse(0, emptyList())
-        else
-            ServiceResponse(1, emptyList())
+        return searchClient.retrieveResults(queryParam)?.let {response ->
+            if (response.isbnEntries.isEmpty())
+                ServiceResponse(0, emptyList())
+            else
+                ServiceResponse(1, listOf(BookEntry("","","")))
+        }
     }
 }
--- test/io/gumichan01/gakusci/domain/PenguinRandomHouseSearchServiceTest.kt
+++ test/io/gumichan01/gakusci/domain/PenguinRandomHouseSearchServiceTest.kt
@@ -1,25 +1,42 @@
 package io.gumichan01.gakusci.domain
 
+import io.gumichan01.gakusci.client.IClient
+import io.gumichan01.gakusci.client.penguin.PenguinRandomHouseSearchResponse
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.gumichan01.gakusci.domain.model.ServiceResponse
 import io.gumichan01.gakusci.domain.service.IService
 import io.gumichan01.gakusci.domain.utils.SearchType
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions
 import kotlin.test.Test
 
 class PenguinRandomHouseSearchServiceTest {
 
+    private val searchClient: IClient<PenguinRandomHouseSearchResponse> = mockk {
+        coEvery { retrieveResults(QueryParam("lorem", SearchType.BOOKS)) } returns
+                PenguinRandomHouseSearchResponse(listOf(""))
+        coEvery {
+            retrieveResults(
+                QueryParam(
+                    "9780140043204",
+                    SearchType.BOOKS
+                )
+            )
+        } returns PenguinRandomHouseSearchResponse(emptyList())
+    }
+
     @Test
     fun `Send valid search request - returns results`() {
-        val service: IService = PenguinRandomHouseSearchService()
+        val service: IService = PenguinRandomHouseSearchService(searchClient)
         val result: ServiceResponse? = runBlocking { service.search(QueryParam("lorem", SearchType.BOOKS)) }
         Assertions.assertThat(result).isNotNull
     }
 
     @Test
     fun `Send valid search request but get no result - returns response with no entry`() {
-        val service: IService = PenguinRandomHouseSearchService()
+        val service: IService = PenguinRandomHouseSearchService(searchClient)
         val result: ServiceResponse? = runBlocking { service.search(QueryParam("9780140043204", SearchType.BOOKS)) }
         Assertions.assertThat(result).isNotNull
         Assertions.assertThat(result?.totalResults).isZero()
