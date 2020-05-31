62012e7d47184256026d2fb83ef2835307006143
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-05-31 15:07:18 +0200

    Set the Jikan manga entry as a data class

--- src/io/gumichan01/gakusci/client/jikan/JikanMangaEntry.kt
+++ src/io/gumichan01/gakusci/client/jikan/JikanMangaEntry.kt
@@ -2,7 +2,7 @@ package io.gumichan01.gakusci.client.jikan
 
 import io.gumichan01.gakusci.domain.utils.DateInterval
 
-class JikanMangaEntry(
+data class JikanMangaEntry(
     val title: String,
     val url: String,
     val publicationPeriod: DateInterval,
