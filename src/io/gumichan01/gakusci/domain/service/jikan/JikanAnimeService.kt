9f3fcbe151c6b50ff0ba48273acde3312a74d2ae
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-05-31 15:22:48 +0200

    Set the cache in the Jikan anime service

--- src/io/gumichan01/gakusci/domain/service/jikan/JikanAnimeService.kt
+++ src/io/gumichan01/gakusci/domain/service/jikan/JikanAnimeService.kt
@@ -5,15 +5,22 @@ import io.gumichan01.gakusci.client.jikan.JikanAnimeResponse
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.gumichan01.gakusci.domain.model.ServiceResponse
 import io.gumichan01.gakusci.domain.model.entry.AnimeEntry
+import io.gumichan01.gakusci.domain.search.cache.CacheHandler
+import io.gumichan01.gakusci.domain.search.cache.SearchCache
 import io.gumichan01.gakusci.domain.service.IService
 
 class JikanAnimeService(val jikanClient: IClient<JikanAnimeResponse>) : IService {
+
+    private val cache: SearchCache = CacheHandler().createFreshCache()
+
     override suspend fun search(queryParam: QueryParam): ServiceResponse? {
-        return jikanClient.retrieveResults(queryParam)?.let { response ->
-            val results = response.entries.map { entry ->
-                AnimeEntry(entry.title, entry.episodes, entry.url, entry.imageUrl ?: "")
+        return cache.getOrUpdateCache(queryParam) {
+            jikanClient.retrieveResults(queryParam)?.let { response ->
+                val results = response.entries.map { entry ->
+                    AnimeEntry(entry.title, entry.episodes, entry.url, entry.imageUrl ?: "")
+                }
+                ServiceResponse(results.size, results)
             }
-            ServiceResponse(results.size, results)
         }
     }
 }
