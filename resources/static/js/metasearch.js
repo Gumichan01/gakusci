8a0f1b30552cb320e5212bc33d069fb2ec2caf8e
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-06-13 12:11:06 +0200

    Clean comment in the JS

--- resources/static/js/metasearch.js
+++ resources/static/js/metasearch.js
@@ -6,15 +6,17 @@ var mangaServices = ["!manga", "!kitsumangas"]
 var animeServices = ["!anidb", "!kitsuanime"]
 
 function autocomplete(inp, research, books, mangas, animes) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
+  /* The autocomplete function takes:
+    - the text field element (the input)
+    - several array of possible autocompleted values, depending of the domain (research, books, ...)
+   */
   var currentFocus;
-  /*execute a function when someone writes in the text field:*/
+  // execute a function when someone writes in the text field
   inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
+      var a, b, i, text = this.value;
       /*close any already open lists of autocompleted values*/
       closeAllLists();
-      if (!val) { return false;}
+      if (!text) { return false;}
       currentFocus = -1;
 
       // Select the list of services related to the type of search
@@ -34,50 +36,37 @@ function autocomplete(inp, research, books, mangas, animes) {
         arr = animes
       }
 
-      /*create a DIV element that will contain the items (values):*/
+      // Display the autocompletion list
       a = document.createElement("DIV");
       a.setAttribute("id", this.id + "autocomplete-list");
       a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
+
       this.parentNode.appendChild(a);
-      /*for each item in the array...*/
       for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
+        if (arr[i].substr(0, text.length).toUpperCase() == text.toUpperCase()) {
           b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, text.length) + "</strong>";
+          b.innerHTML += arr[i].substr(text.length);
           b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
+
+          // I integrate this listener in order to update the selected entry in the autocompletion list
+          b.addEventListener("click", function(e) {
+            inp.value = this.getElementsByTagName("input")[0].value;
+            closeAllLists();
+           });
           a.appendChild(b);
         }
       }
   });
-  /*execute a function presses a key on the keyboard:*/
+  // execute a function presses a key on the keyboard
   inp.addEventListener("keydown", function(e) {
       var x = document.getElementById(this.id + "autocomplete-list");
       if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
+      if (e.keyCode == 40) {    // DOWN key pressed
         currentFocus++;
-        /*and and make the current item more visible:*/
         addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
+      } else if (e.keyCode == 38) { // UP key pressed
         currentFocus--;
-        /*and and make the current item more visible:*/
         addActive(x);
       }
   });
@@ -85,11 +74,9 @@ function autocomplete(inp, research, books, mangas, animes) {
   function addActive(x) {
     /*a function to classify an item as "active":*/
     if (!x) return false;
-    /*start by removing the "active" class on all items:*/
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
 
@@ -111,7 +98,7 @@ function autocomplete(inp, research, books, mangas, animes) {
     }
   }
 
-/*execute a function when someone clicks in the document:*/
+// execute a function when someone clicks in the document:
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
 });
