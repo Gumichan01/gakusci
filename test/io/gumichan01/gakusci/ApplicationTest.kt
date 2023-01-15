adb2ad6381a44d1b93989d9828eef7d8ef766672
Gumichan01 <luxon.jean.pierre@gmail.com>
2023-01-15 20:30:29 +0100

    Fix deprecation warnings
    Use TestApplication instead of the depreceted withTestApplication.
    
    ```
    'withTestApplication(Application.() -> Unit, TestApplicationEngine.() -> R): R' is deprecated.
    Please use new `testApplication` API: https://ktor.io/docs/migrating-2.html#testing-api
    ```

--- test/io/gumichan01/gakusci/ApplicationTest.kt
+++ test/io/gumichan01/gakusci/ApplicationTest.kt
@@ -1,10 +1,10 @@
 package io.gumichan01.gakusci
 
 import io.gumichan01.gakusci.controller.utils.MAX_ENTRIES
-import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.testing.handleRequest
-import io.ktor.server.testing.withTestApplication
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import org.assertj.core.api.Assertions.assertThat
@@ -15,181 +15,174 @@ import kotlin.test.Test
 class ApplicationTest {
     @Test
     fun `webapp, static homepage - returns OK`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
+        testApplication {
+            val response = client.get("/")
+            assertThat(response.status).isEqualTo(HttpStatusCode.OK)
+        }
+    }
+
+    @Test
+    fun `webapp, normal case with research - returns OK and HTML body`() {
+        testApplication {
+            val response = client.get("/search/?q=lorem&stype=research")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.OK)
+                assertThat(bodyAsText()).isNotBlank
+                assertThat(bodyAsText()).contains(
+                    listOf(
+                        "<html>",
+                        "<div>",
+                        "</div>",
+                        "<script src=",
+                        "</script>",
+                        "</html>"
+                    )
+                )
             }
         }
     }
 
     @Test
-    fun `webapp, normal case with research - returns OK and HTML content`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=lorem&stype=research").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.OK)
-                    assertThat(content).isNotBlank()
-                    assertThat(content).contains(
-                        listOf(
-                            "<html>",
-                            "<div>",
-                            "</div>",
-                            "<script src=",
-                            "</script>",
-                            "</html>"
-                        )
+    fun `webapp, normal case with books - returns OK and HTML body`() {
+        testApplication {
+            val response = client.get("/search/?q=lorem&stype=books")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.OK)
+                assertThat(bodyAsText()).isNotBlank
+                assertThat(bodyAsText()).contains(
+                    listOf(
+                        "<html>",
+                        "<div>",
+                        "</div>",
+                        "<script src=",
+                        "</script>",
+                        "</html>"
                     )
-                }
+                )
             }
         }
     }
 
     @Test
-    fun `webapp, normal case with books - returns OK and HTML content`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=lorem&stype=books").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.OK)
-                    assertThat(content).isNotBlank()
-                    assertThat(content).contains(
-                        listOf(
-                            "<html>",
-                            "<div>",
-                            "</div>",
-                            "<script src=",
-                            "</script>",
-                            "</html>"
-                        )
+    fun `webapp, search for a book by ISBN - returns OK and HTML body`() {
+        testApplication {
+            val response = client.get("/search/?q=ISBN:9784088766829&stype=books")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.OK)
+                assertThat(bodyAsText()).isNotBlank
+                assertThat(bodyAsText()).contains(
+                    listOf(
+                        "<html>",
+                        "<div>",
+                        "ISBN",
+                        "9784088766829",
+                        "</div>",
+                        "<script src=",
+                        "</script>",
+                        "</html>"
                     )
-                }
-            }
-        }
-    }
-
-    @Test
-    fun `webapp, search for a book by ISBN - returns OK and HTML content`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=ISBN:9784088766829&stype=books").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.OK)
-                    assertThat(content).isNotBlank()
-                    assertThat(content).contains(
-                        listOf(
-                            "<html>",
-                            "<div>",
-                            "ISBN",
-                            "9784088766829",
-                            "</div>",
-                            "<script src=",
-                            "</script>",
-                            "</html>"
-                        )
-                    )
-                }
+                )
             }
         }
     }
 
     @Test
     fun `webapp, query with negative start value - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=lorem&stype=research&start=-10").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/search/?q=lorem&stype=research&start=-10")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `webapp, empty query - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=&stype=research").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.BadRequest)
-                }
+        testApplication {
+            val response = client.get("/search/?q=&stype=research")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
     }
 
     @Test
     fun `webapp, blank query - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=     &stype=research").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.BadRequest)
-                }
+        testApplication {
+            val response = client.get("/search/?q=     &stype=research")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
     }
 
     @Test
     fun `webapp, query too short (less than 3 characters) - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=a&stype=research").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.BadRequest)
-                }
+        testApplication {
+            val response = client.get("/search/?q=a&stype=research")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
     }
+
 
     @Test
     fun `webapp, no search type for a query - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=science").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.BadRequest)
-                }
+        testApplication {
+            val response = client.get("/search/?q=science")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
     }
+
 
     @Test
     fun `test web application search, query with very big start value - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
+        testApplication {
             val bigStartValue = 1 shl 20
-            handleRequest(HttpMethod.Get, "/search/?q=lorem&stype=research&start=$bigStartValue").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+            val response = client.get("/search/?q=lorem&stype=research&start=$bigStartValue")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
+
     @Test
-    fun `Query 'fruit' returning more than 2000 results (10 entries per page) - returns HTML content with 'start' value of the last page less than 1990`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=fruit&stype=research").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
-                assertThat(response.content?.substringAfterLast("start=")?.substringBefore("\"")?.toInt())
-                    .isEqualTo(MAX_ENTRIES - 10)
-            }
+    fun `Query 'fruit' returning more than 2000 results (10 entries per page) - returns HTML body with 'start' value of the last page less than 1990`() {
+        testApplication {
+            val response = client.get("/search/?q=fruit&stype=research")
+            assertThat(response.status).isEqualTo(HttpStatusCode.OK)
+            assertThat(response.bodyAsText().substringAfterLast("start=").substringBefore("\"").toInt())
+                .isEqualTo(MAX_ENTRIES - 10)
         }
     }
 
+
     @Test
-    fun `Query 'lorem' that returns less than 2000 results (10 entries per page) - returns HTML content with 'start' value of the last page less than 1990`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=ipsum&stype=research").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
-                assertThat(response.content?.substringAfterLast("start=")?.substringBefore("\"")?.toInt())
-                    .isLessThan(MAX_ENTRIES - 10)
-            }
+    fun `Query 'lorem' that returns less than 2000 results (10 entries per page) - returns HTML body with 'start' value of the last page less than 1990`() {
+        testApplication {
+            val response = client.get("/search/?q=ipsum&stype=research")
+            assertThat(response.status).isEqualTo(HttpStatusCode.OK)
+            assertThat(response.bodyAsText().substringAfterLast("start=").substringBefore("\"").toInt())
+                .isLessThan(MAX_ENTRIES - 10)
         }
     }
+
 
     @Test
     fun `Redirect query - return redirect`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=!hal").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.Found)
-            }
+        testApplication {
+            val response = client.get("/search/?q=!hal")
+            assertThat(response.status).isEqualTo(HttpStatusCode.Found)
         }
     }
 
+
     @Test
     fun `Redirect query with bad bang request - return bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/search/?q=!invalidbang").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/search/?q=!invalidbang")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
+
 }
--- test/io/gumichan01/gakusci/RestApiTest.kt
+++ test/io/gumichan01/gakusci/RestApiTest.kt
@@ -1,9 +1,9 @@
 package io.gumichan01.gakusci
 
-import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.testing.handleRequest
-import io.ktor.server.testing.withTestApplication
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import org.assertj.core.api.Assertions.assertThat
@@ -14,170 +14,145 @@ import kotlin.test.Test
 class RestApiTest {
     @Test
     fun `test REST Web-service API v1 search, normal case - return OK and non empty content`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=lorem").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
-                assertThat(response.content).isNotBlank()
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=lorem")
+            assertThat(response.status).isEqualTo(HttpStatusCode.OK)
+            assertThat(response.bodyAsText()).isNotBlank()
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, no query - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, blank query - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=     ").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.BadRequest)
-                }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=     ")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
     }
 
     @Test
     fun `webapp, query too short (less than 3 characters) - returns Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=a").apply {
-                with(response) {
-                    assertThat(status()).isEqualTo(HttpStatusCode.BadRequest)
-                }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=a")
+            with(response) {
+                assertThat(status).isEqualTo(HttpStatusCode.BadRequest)
             }
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, query with start but no num_per_page - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcd&start=0").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcd&start=0")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, query with num_per_page but no start - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcd&num_per_page=2").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcd&num_per_page=2")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, query with start but no max_results - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcd&start=0").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcd&start=0")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
+
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, query with pagination (start, num_per_page, max_results) - return ok`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcd&start=0&num_per_page=4&max_results=16").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.OK)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcd&start=0&num_per_page=4&max_results=16")
+            assertThat(response.status).isEqualTo(HttpStatusCode.OK)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, query with start greater than max_results - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcda&max_results=10&start=100&num_per_page=2").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcda&max_results=10&start=100&num_per_page=2")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, query with negative start value - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcda&start=-10&max_results=16&num_per_page=2").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcda&start=-10&max_results=16&num_per_page=2")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, pagination but with negative max_results - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcdb&max_results=-1&start=0&num_per_page=2").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcdb&max_results=-1&start=0&num_per_page=2")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, negative max_results without pagination - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcdb&max_results=-1").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcdb&max_results=-1")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, negative num_per_page - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=abcdb&max_results=10&start=0&num_per_page=-1").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=abcdb&max_results=10&start=0&num_per_page=-1")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST Web-service API v1 search, query with numPerPage greater than max_results - return Bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(
-                HttpMethod.Get,
-                "/api/v1/researches/?q=babcd&max_results=10&start=1&num_per_page=100000"
-            ).apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=babcd&max_results=10&start=1&num_per_page=100000")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST API v1, max_results greater than the maximum number of entries allowed by the app - Bad request`() {
-        withTestApplication({ gakusciModule() }) {
+        testApplication {
             val bigStartValue = 1 shl 20
-            handleRequest(
-                HttpMethod.Get,
-                "/api/v1/researches/?q=aabcd&start=0&max_results=$bigStartValue&num_per_page=2"
-            ).apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+            val response = client.get("/api/v1/researches/?q=aabcd&start=0&max_results=$bigStartValue&num_per_page=2")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `test REST API v1, max_results greater than the maximum number of entries allowed by the app (no start param) - Bad request`() {
-        withTestApplication({ gakusciModule() }) {
+        testApplication {
             val bigStartValue = 1 shl 20
-            handleRequest(
-                HttpMethod.Get,
-                "/api/v1/researches/?q=aabcd&max_results=$bigStartValue"
-            ).apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+            val response = client.get("/api/v1/researches/?q=aabcd&max_results=$bigStartValue")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 
     @Test
     fun `Redirect query in REST API - return bad request`() {
-        withTestApplication({ gakusciModule() }) {
-            handleRequest(HttpMethod.Get, "/api/v1/researches/?q=!hal").apply {
-                assertThat(response.status()).isEqualTo(HttpStatusCode.BadRequest)
-            }
+        testApplication {
+            val response = client.get("/api/v1/researches/?q=!hal")
+            assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         }
     }
 }
