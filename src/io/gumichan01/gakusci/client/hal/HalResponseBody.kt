d1f2cd889f2b7b187d023f35847944cde10d53af
Gumichan01 <luxon.jean.pierre@gmail.com>
2023-01-21 20:50:08 +0100

    Ignore extra properties of data from HAL

--- src/io/gumichan01/gakusci/client/hal/HalResponseBody.kt
+++ src/io/gumichan01/gakusci/client/hal/HalResponseBody.kt
@@ -1,3 +1,6 @@
 package io.gumichan01.gakusci.client.hal
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class HalResponseBody(val numFound: Int, val start: Int, val docs: List<HalResultEntry>?)
