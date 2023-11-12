26f93d3555f69a8acb31e3f30684533c8b66ed29
Gumichan01 <luxon.jean.pierre@gmail.com>
2023-11-12 21:32:24 +0100

    Replace old Jikan client implementation.
    Jikan API was updated to v4. JikanKt, the library I use to interact with Jikan, does not support this new version.
    JikanKt was also archived and is not updated anymore.
    
    Consequently, I had to replace it with Jaikan, which support Jikan v4.
    
    Link:
    
    - https://github.com/ShindouMihou/Jaikan

--- src/io/gumichan01/gakusci/client/jikan/JikanAnimeClient.kt
+++ src/io/gumichan01/gakusci/client/jikan/JikanAnimeClient.kt
@@ -6,32 +6,42 @@ import io.github.bucket4j.Refill
 import io.github.bucket4j.local.LocalBucket
 import io.gumichan01.gakusci.client.IClient
 import io.gumichan01.gakusci.domain.model.QueryParam
-import moe.ganen.jikankt.JikanKt
-import moe.ganen.jikankt.models.search.AnimeSearchQuery
-import java.time.Duration
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import pw.mihou.jaikan.Jaikan
+import pw.mihou.jaikan.endpoints.Endpoints
+import pw.mihou.jaikan.models.Anime
+import java.net.URLEncoder
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
 class JikanAnimeClient : IClient<JikanAnimeResponse> {
 
     private val rateLimiter: LocalBucket by lazy {
         Bucket.builder()
-            .addLimit(Bandwidth.classic(2, Refill.greedy(1L, Duration.ofMillis(500))))
-            .build()
+                .addLimit(Bandwidth.classic(2, Refill.greedy(1L, 500.milliseconds.toJavaDuration())))
+                .build()
     }
 
     override suspend fun retrieveResults(queryParam: QueryParam): JikanAnimeResponse? {
+        val requestTimeout: Duration = 5.seconds
         return if (rateLimiter.tryConsume(1L)) {
-            JikanKt.searchAnime(queryParam.query, AnimeSearchQuery(limit = queryParam.rows)).results
-                ?.let {
-                    val entries: List<JikanAnimeEntry> = it.asSequence()
-                        .filterNotNull()
-                        .filter { e -> e.title != null }
-                        .filter { e -> e.url != null }
-                        .map { entry ->
-                            JikanAnimeEntry(entry.title!!, entry.url!!, entry.episodes, entry.imageUrl)
-                        }
-                        .toList()
-                    JikanAnimeResponse(entries)
-                }
+            val entries: List<JikanAnimeEntry> = withContext(Dispatchers.IO) {
+                Jaikan.list(Endpoints.SEARCH, Anime::class.java, "anime", URLEncoder.encode(queryParam.query, Charsets.UTF_8))
+                        .thenApply { animeList ->
+                            animeList.asSequence()
+                                    .filterNotNull()
+                                    .filter { e -> e.title != null }
+                                    .filter { e -> e.url != null }
+                                    .map { entry ->
+                                        JikanAnimeEntry(entry.title!!, entry.url!!, entry.episodes, entry.images.firstDefault())
+                                    }.take(queryParam.rows).toList()
+                        }.get(requestTimeout.inWholeSeconds, TimeUnit.SECONDS)
+            }
+            JikanAnimeResponse(entries)
         } else null
     }
 }
--- src/io/gumichan01/gakusci/client/jikan/JikanMangaClient.kt
+++ src/io/gumichan01/gakusci/client/jikan/JikanMangaClient.kt
@@ -7,37 +7,48 @@ import io.github.bucket4j.local.LocalBucket
 import io.gumichan01.gakusci.client.IClient
 import io.gumichan01.gakusci.domain.model.QueryParam
 import io.gumichan01.gakusci.domain.utils.DateInterval
-import moe.ganen.jikankt.JikanKt
-import moe.ganen.jikankt.models.search.MangaSearchQuery
-import java.time.Duration
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import pw.mihou.jaikan.Jaikan
+import pw.mihou.jaikan.endpoints.Endpoints
+import pw.mihou.jaikan.models.Manga
+import java.net.URLEncoder
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
 class JikanMangaClient : IClient<JikanMangaResponse> {
 
     private val rateLimiter: LocalBucket by lazy {
         Bucket.builder()
-            .addLimit(Bandwidth.classic(2, Refill.greedy(1L, Duration.ofMillis(500))))
-            .build()
+                .addLimit(Bandwidth.classic(2, Refill.greedy(1L, 500.milliseconds.toJavaDuration())))
+                .build()
     }
 
     override suspend fun retrieveResults(queryParam: QueryParam): JikanMangaResponse? {
+        val requestTimeout: Duration = 5.seconds
         return if (rateLimiter.tryConsume(1L)) {
-            JikanKt.searchManga(queryParam.query, MangaSearchQuery(limit = queryParam.rows)).results
-                ?.let {
-                    val entries: List<JikanMangaEntry> = it.asSequence()
-                        .filterNotNull()
-                        .filter { e -> e.title != null }
-                        .filter { e -> e.url != null }
-                        .filter { e -> e.startDate != null }
-                        .map { entry ->
-                            JikanMangaEntry(
-                                entry.title!!,
-                                entry.url!!,
-                                DateInterval(entry.startDate!!, entry.endDate),
-                                entry.imageUrl
-                            )
-                        }.toList()
-                    JikanMangaResponse(entries)
-                }
+            val entries: List<JikanMangaEntry> = withContext(Dispatchers.IO) {
+                Jaikan.list(Endpoints.SEARCH, Manga::class.java, "manga", URLEncoder.encode(queryParam.query, Charsets.UTF_8))
+                        .thenApply { mangaList ->
+                            mangaList.asSequence()
+                                    .filterNotNull()
+                                    .filter { e -> e.title != null }
+                                    .filter { e -> e.url != null }
+                                    .filter { e -> e.published.from != null }
+                                    .map { entry ->
+                                        JikanMangaEntry(
+                                                entry.title!!,
+                                                entry.url!!,
+                                                DateInterval(entry.published.from!!, entry.published.to),
+                                                entry.images.firstDefault()
+                                        )
+                                    }.take(queryParam.rows).toList()
+                        }.get(requestTimeout.inWholeSeconds, TimeUnit.SECONDS)
+            }
+            JikanMangaResponse(entries)
         } else null
     }
 }
