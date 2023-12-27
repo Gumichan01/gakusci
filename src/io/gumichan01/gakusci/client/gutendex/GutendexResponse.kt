7011f01d29cd49a14526978113061133196b631b
Gumichan01 <luxon.jean.pierre@gmail.com>
2023-12-27 18:27:12 +0100

    Set nullable data in gutendex response
    The next page is null, when the current page is the last one.

--- src/io/gumichan01/gakusci/client/gutendex/GutendexResponse.kt
+++ src/io/gumichan01/gakusci/client/gutendex/GutendexResponse.kt
@@ -2,7 +2,7 @@ package io.gumichan01.gakusci.client.gutendex
 
 data class GutendexResponse(
     val count: Int,
-    val next: String,
+    val next: String?,
     val previous: String?,
     val results: List<GutendexBookEntry>
 )
