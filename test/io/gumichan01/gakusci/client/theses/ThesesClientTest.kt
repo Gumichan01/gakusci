c300bd371e9083adb37019e32144caf94d7ad61b
Gumichan01 <luxon.jean.pierre@gmail.com>
2023-12-27 21:35:44 +0100

    Update tests for Theses

--- test/io/gumichan01/gakusci/client/theses/ThesesClientTest.kt
+++ test/io/gumichan01/gakusci/client/theses/ThesesClientTest.kt
@@ -4,17 +4,21 @@ import io.gumichan01.gakusci.domain.model.QueryParam
 import io.gumichan01.gakusci.domain.utils.SearchType
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions
+import org.junit.Test
 import kotlin.test.assertTrue
 
 class ThesesClientTest {
 
-//    @Test
+    //@Test
     fun `Call the Theses service - get results`() {
         val response = runBlocking { ThesesClient().retrieveResults(QueryParam("coroutine", SearchType.RESEARCH)) }
         Assertions.assertThat(response).isNotNull
+        Assertions.assertThat(response!!.body.numFound).isPositive
+        Assertions.assertThat(response.body.start).isZero
+        Assertions.assertThat(response.body.docs.size).isPositive
     }
 
-//    @Test
+    //@Test
     fun `Call the Theses service with query containing spaces - should not fail `() {
         runBlocking { ThesesClient().retrieveResults(QueryParam("gunnm last order", SearchType.RESEARCH)) }
         assertTrue { true } // No need to check. The request must not fail in order to validate the test
