0a9f58624f0aa5aba979232b34decb10cc724036
Gumichan01 <luxon.jean.pierre@gmail.com>
2023-12-29 20:03:51 +0100

    Add test for Arxiv cache

---
+++ test/io/gumichan01/gakusci/client/arxiv/ArxivCacheTest.kt
@@ -0,0 +1,54 @@
+package io.gumichan01.gakusci.client.arxiv
+
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
+import io.gumichan01.gakusci.domain.model.QueryParam
+import io.gumichan01.gakusci.domain.utils.SearchType
+import io.mockk.EqMatcher
+import io.mockk.coEvery
+import io.mockk.core.ValueClassSupport.boxedValue
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.sql.Date
+
+internal class ArxivCacheTest {
+
+    private val fakeDate = Date.valueOf("2023-01-01")
+    private val expectedResponse = ArxivResponse(1, listOf(ArxivResultEntry(emptyList(), "test-arxiv", fakeDate, "///fake/path")))
+    private val arxivClientMock: ArxivClient = mockk {
+        coEvery { retrieveResults(QueryParam("loremi", SearchType.RESEARCH)) } returns expectedResponse
+    }
+
+    @Test
+    fun `Arxiv Cache - Save response, expect cached data`() {
+        val cache = ArxivCache()
+        val expectedValue = ArxivResponse(0, emptyList())
+        cache.put("lorem", ArxivResponse(0, emptyList()))
+        val value: ArxivResponse = cache.get("lorem") {
+            ArxivResponse(1, listOf(ArxivResultEntry(emptyList(), "FAIL TEST", fakeDate, "")))
+        }
+        assertThat(value).isEqualTo(expectedValue)
+        assertThat(value.numFound).isZero
+        assertThat(value.docs).isEmpty()
+    }
+
+    @Test
+    fun `Arxiv Cache - Retrieve data, cache it, retrieve it again, expect cached same data`() {
+        val cache = ArxivCache()
+        val resp01: ArxivResponse = cache.get("loremi") {
+            runBlocking { arxivClientMock.retrieveResults(QueryParam("loremi", SearchType.RESEARCH)) }
+        }
+        val resp02: ArxivResponse = cache.get("loremi") {
+            runBlocking { arxivClientMock.retrieveResults(QueryParam("loremi", SearchType.RESEARCH)) }
+        }
+        val value: ArxivResponse = cache.get("loremi") {
+            ArxivResponse(1, listOf(ArxivResultEntry(emptyList(), "FAIL TEST", fakeDate, "")))
+        }
+        assertThat(value).isEqualTo(expectedResponse)
+        assertThat(value).isEqualTo(resp01)
+        assertThat(value).isEqualTo(resp02)
+    }
+}
