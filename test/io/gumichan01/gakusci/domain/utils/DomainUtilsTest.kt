a28ffac986ff17ee09f4bfc93e9754dc79a9451a
Gumichan01 <luxon.jean.pierre@gmail.com>
2024-01-01 19:26:33 +0100

    Rename test file

--- test/io/gumichan01/gakusci/domain/utils/DomainUtilsTest.kt
+++ test/io/gumichan01/gakusci/domain/utils/DomainUtilsTest.kt
@@ -6,79 +6,61 @@ import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 
 
-internal class DomainUtilsKtTest {
+internal class DomainUtilsTest {
 
     @Test
-    fun `Take elements from list`(): Unit {
+    fun `Take elements from list`() {
         val response = SearchResponse(
             1, 0, listOf(
-                SimpleResultEntry(
-                    "",
-                    ""
-                )
-            )
-        )
-        assertThat(response.take(1).entries).contains(
             SimpleResultEntry(
                 "",
                 ""
-            )
-        )
+            )))
+        assertThat(response.take(1).entries).contains(
+            SimpleResultEntry(
+                "",
+                ""))
     }
 
     @Test
     fun `Retrieve sublist of list - normal case`(): Unit {
         val response = SearchResponse(
             1, 0, listOf(
-                SimpleResultEntry(
-                    "",
-                    ""
-                )
-            )
-        )
+            SimpleResultEntry(
+                "",
+                "")))
         assertThat(response.slice(IntRange(0, 0)).entries).contains(
             SimpleResultEntry(
                 "",
-                ""
-            )
-        )
+                ""))
     }
 
     @Test
     fun `Retrieve sublist bigger than the list - return the list `() {
         val response = SearchResponse(
             2, 0, listOf(
-                SimpleResultEntry(
-                    "a",
-                    "b"
-                ), SimpleResultEntry("", "")
-            )
-        )
+            SimpleResultEntry(
+                "a",
+                "b"
+            ), SimpleResultEntry("", "")))
         assertThat(response.slice(IntRange(0, 42)).entries).isEqualTo(
             listOf(
                 SimpleResultEntry("a", "b"),
-                SimpleResultEntry("", "")
-            )
-        )
+                SimpleResultEntry("", "")))
     }
 
     @Test
     fun `Retrieve sublist at the end of the list with non-zero start - return the sublist `() {
         val response = SearchResponse(
             2, 0, listOf(
-                SimpleResultEntry(
-                    "a",
-                    "b"
-                ), SimpleResultEntry("", "")
-            )
-        )
+            SimpleResultEntry(
+                "a",
+                "b"
+            ), SimpleResultEntry("", "")))
         assertThat(response.slice(IntRange(1, 4)).entries).isEqualTo(
             listOf(
                 SimpleResultEntry(
                     "",
-                    ""
-                )
-            )
-        )
+                    "")))
     }
 }
