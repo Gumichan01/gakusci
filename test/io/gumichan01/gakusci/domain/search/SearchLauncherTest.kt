49d97b2df21987fad7c0e186e608a33145e18a32
Gumichan01 <luxon.jean.pierre@gmail.com>
2024-01-01 20:48:18 +0100

    Refactorize search launcher test

--- test/io/gumichan01/gakusci/domain/search/SearchLauncherTest.kt
+++ test/io/gumichan01/gakusci/domain/search/SearchLauncherTest.kt
@@ -19,34 +19,30 @@ import kotlin.test.Test
 class SearchLauncherTest {
 
     private val fakeService: IService = mockk {
-        coEvery { search(QueryParam("lorem", SearchType.RESEARCH, )) } returns ServiceResponse(
+        coEvery { search(QueryParam("lorem", SearchType.RESEARCH)) } returns ServiceResponse(
             1,
-            listOf(SimpleResultEntry("", ""))
-        )
+            listOf(SimpleResultEntry("", "")))
     }
     private val fakeService2: IService = mockk {
-        coEvery { search(QueryParam("lorem", SearchType.RESEARCH, )) } returns ServiceResponse(
+        coEvery { search(QueryParam("lorem", SearchType.RESEARCH)) } returns ServiceResponse(
             1,
             listOf(SimpleResultEntry("", ""))
         )
     }
     private val fakeObjectService: IService = mockk {
-        coEvery { search(QueryParam("lorem", SearchType.RESEARCH, )) } returns
-                ServiceResponse(
-                    1, listOf(
-                        SimpleResultEntry(
-                            "lorem",
-                            "ipsum"
-                        )
-                    )
-                )
+        coEvery { search(QueryParam("lorem", SearchType.RESEARCH)) } returns
+            ServiceResponse(
+                1, listOf(
+                SimpleResultEntry(
+                    "lorem",
+                    "ipsum")))
     }
 
 
     @Test
     fun `launch request with no service - return closed channel`() {
         val searchLauncher = SearchLauncher(emptySet())
-        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH, ))
+        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH))
         assertThat(channel.isClosedForReceive).isTrue
         assertThat(channel.isClosedForSend).isTrue
     }
@@ -54,50 +50,40 @@ class SearchLauncherTest {
     @Test
     fun `launch request with one fake service - return non-closed channel`() {
         val searchLauncher = SearchLauncher(setOf(fakeService))
-        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH, ))
+        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH))
         assertThat(channel.isClosedForReceive).isFalse
     }
 
     @Test
     fun `launch request with several fake services - return non-empty channel and get data`() {
         val searchLauncher = SearchLauncher(setOf(fakeService, fakeService2))
-        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH, ))
+        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH))
         assertThat(runBlocking { channel.receive() })
             .isEqualTo(
                 ServiceResponse(
                     1, listOf(
-                        SimpleResultEntry(
-                            "",
-                            ""
-                        )
-                    )
-                )
-            )
+                    SimpleResultEntry(
+                        "",
+                        ""))))
     }
 
     @Test
     fun `launch request with several fake services - retrieve data`() {
         val searchLauncher = SearchLauncher(setOf(fakeService, fakeObjectService))
-        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH, ))
+        val channel: Channel<ServiceResponse> = searchLauncher.launch(QueryParam("lorem", SearchType.RESEARCH))
 
         val expectedResults: Set<ServiceResponse> = setOf(
             ServiceResponse(
                 1, listOf(
-                    SimpleResultEntry(
-                        "",
-                        ""
-                    )
-                )
-            ),
+                SimpleResultEntry(
+                    "",
+                    ""))),
             ServiceResponse(
                 1, listOf(
-                    SimpleResultEntry(
-                        "lorem",
-                        "ipsum"
-                    )
-                )
-            )
-        )
+                SimpleResultEntry(
+                    "lorem",
+                    "ipsum"))))
+
         val tmpResults: MutableList<ServiceResponse> = mutableListOf()
         repeat(2) {
             tmpResults += runBlocking { channel.receive() }
