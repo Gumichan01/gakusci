10187071fcd1cfe076ea7e31bc70049c6eddb1c9
Gumichan01 <luxon.jean.pierre@gmail.com>
2024-01-01 20:11:01 +0100

    Remove useless fields in the RequestParam

--- src/io/gumichan01/gakusci/controller/utils/RequestParam.kt
+++ src/io/gumichan01/gakusci/controller/utils/RequestParam.kt
@@ -5,6 +5,6 @@ import io.gumichan01.gakusci.domain.utils.SearchType
 data class RequestParam(
     val query: String,
     val searchType: SearchType,
-    val rows: Int = 10,
-    val start: Int = 0
+    val rows: Int,
+    val start: Int
 ) : IRequestParamResult
