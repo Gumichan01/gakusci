5b4f3909cecd9fec8a9f0d93a028d3cab35c821e
Gumichan01 <luxon.jean.pierre@gmail.com>
2024-01-02 12:59:47 +0100

    Add new test for Arxiv client: handle queries containing spaces
    Handle a different result type

--- test/io/gumichan01/gakusci/client/arxiv/ArxivClientTest.kt
+++ test/io/gumichan01/gakusci/client/arxiv/ArxivClientTest.kt
@@ -5,23 +5,34 @@ import io.gumichan01.gakusci.domain.model.SimpleQuery
 import io.gumichan01.gakusci.domain.utils.SearchType
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
 
 class ArxivClientTest {
 
     //@Test
     fun `Call the Arxiv service - get results`() {
-        val response: ArxivResponse? = runBlocking { ArxivClient().retrieveResults(SimpleQuery("science")) }
+        val response: ArxivResponse = runBlocking { ArxivClient().retrieveResults(SimpleQuery("science")) }
         assertThat(response).isNotNull
-        assertThat(response!!.docs.size).isGreaterThan(0)
+        assertThat(response.docs.size).isGreaterThan(0)
+        assertThat(response.numFound).isGreaterThan(0)
+    }
+
+    //@Test
+    fun `Call the Arxiv service with query containing spaces - get results`() {
+        val response: ArxivResponse = runBlocking {
+            ArxivClient().retrieveResults(SimpleQuery("book sales"))
+        }
+        assertThat(response).isNotNull
+        assertThat(response.docs.size).isGreaterThan(0)
         assertThat(response.numFound).isGreaterThan(0)
     }
 
     //@Test
     fun `Call the Arxiv service with request that returns no result`() {
-        val response: ArxivResponse? = runBlocking { ArxivClient().retrieveResults(SimpleQuery("azertyu")) }
+        val response: ArxivResponse = runBlocking { ArxivClient().retrieveResults(SimpleQuery("azertyu")) }
         assertThat(response).isNotNull
-        assertThat(response?.numFound).isEqualTo(0)
-        assertThat(response?.docs).isEmpty()
+        assertThat(response.numFound).isEqualTo(0)
+        assertThat(response.docs).isEmpty()
     }
 }
 
