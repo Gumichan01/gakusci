2246cda8d20d4b9cb39935ebcbd7fe91825904fa
Gumichan01 <luxon.jean.pierre@gmail.com>
2024-07-06 19:30:37 +0200

    Do not return a cover of a simple entry
    This is because a misconception.

--- src/io/gumichan01/gakusci/domain/model/entry/SimpleResultEntry.kt
+++ src/io/gumichan01/gakusci/domain/model/entry/SimpleResultEntry.kt
@@ -3,5 +3,5 @@ package io.gumichan01.gakusci.domain.model.entry
 data class SimpleResultEntry(val label: String, val url: String) : IResultEntry {
     override fun label(): String = label
     override fun link(): String = url
-    override fun cover(): String = ""
+    override fun cover(): String = throw NotImplementedError("This entry has no cover() implementation")
 }
