ba2ab6e95244e600a4da5e4a3555871a66fbbdf4
Gumichan01 <luxon.jean.pierre@gmail.com>
2024-09-28 19:21:38 +0200

    Set music services in the completion list

--- resources/static/js/metasearch.js
+++ resources/static/js/metasearch.js
@@ -4,15 +4,16 @@ var researchServices = ["!arxiv", "!hal", "!libgen", "!scihub", "!theses", "!the
 var bookServices = ["!goodreads", "!openlib", "!penguin"]
 var mangaServices = ["!manga", "!kitsumanga"]
 var animeServices = ["!anidb", "!anime", "!anilist", "!kitsuanime"]
+var musicServices = ["!discogs", "!freesound", "!musicbrainz", "!opengameart"]
 
-function autocomplete(inp, research, books, mangas, animes) {
+function autocomplete(queryInput, research, books, mangas, animes, music) {
   /* The autocomplete function takes:
     - the text field element (the input)
     - several array of possible autocompleted values, depending of the domain (research, books, ...)
    */
   var currentFocus;
   // execute a function when someone writes in the text field
-  inp.addEventListener("input", function(e) {
+  queryInput.addEventListener("input", function(e) {
       var a, b, i, text = this.value;
       /*close any already open lists of autocompleted values*/
       closeAllLists();
@@ -24,16 +25,19 @@ function autocomplete(inp, research, books, mangas, animes) {
       var tbooks = document.getElementById("type_books")
       var tmanga = document.getElementById("type_manga")
       var tanime = document.getElementById("type_anime")
-      var arr;
+      var tmusic = document.getElementById("type_music")
+      var completionList;
 
       if (tpapers.checked) {
-        arr = research
+        completionList = research
       } else if (tbooks.checked) {
-        arr = books
+        completionList = books
       } else if (tmanga.checked) {
-        arr = mangas
+        completionList = mangas
       } else if (tanime.checked) {
-        arr = animes
+        completionList = animes
+      } else if (tmusic.checked) {
+      completionList = music
       }
 
       // Display the autocompletion list
@@ -42,16 +46,16 @@ function autocomplete(inp, research, books, mangas, animes) {
       a.setAttribute("class", "autocomplete-items");
 
       this.parentNode.appendChild(a);
-      for (i = 0; i < arr.length; i++) {
-        if (arr[i].substr(0, text.length).toUpperCase() == text.toUpperCase()) {
+      for (i = 0; i < completionList.length; i++) {
+        if (completionList[i].substr(0, text.length).toUpperCase() == text.toUpperCase()) {
           b = document.createElement("DIV");
-          b.innerHTML = "<strong>" + arr[i].substr(0, text.length) + "</strong>";
-          b.innerHTML += arr[i].substr(text.length);
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.innerHTML = "<strong>" + completionList[i].substr(0, text.length) + "</strong>";
+          b.innerHTML += completionList[i].substr(text.length);
+          b.innerHTML += "<input type='hidden' value='" + completionList[i] + "'>";
 
           // I integrate this listener in order to update the selected entry in the autocompletion list
           b.addEventListener("click", function(e) {
-            inp.value = this.getElementsByTagName("input")[0].value;
+            queryInput.value = this.getElementsByTagName("input")[0].value;
             closeAllLists();
            });
           a.appendChild(b);
@@ -59,17 +63,17 @@ function autocomplete(inp, research, books, mangas, animes) {
       }
   });
   // execute a function presses a key on the keyboard
-  inp.addEventListener("keydown", function(e) {
+  queryInput.addEventListener("keydown", function(e) {
       var x = document.getElementById(this.id + "autocomplete-list");
       if (x) x = x.getElementsByTagName("div");
       if (e.keyCode == 40) {    // DOWN key pressed
         currentFocus++;
         addActive(x);
-        inp.value = x[currentFocus].getElementsByTagName("input")[0].value + " "
+        queryInput.value = x[currentFocus].getElementsByTagName("input")[0].value + " "
       } else if (e.keyCode == 38) { // UP key pressed
         currentFocus--;
         addActive(x);
-        inp.value = x[currentFocus].getElementsByTagName("input")[0].value + " "
+        queryInput.value = x[currentFocus].getElementsByTagName("input")[0].value + " "
       } else if (e.keyCode == 27) { // ESCAPE key pressed
         closeAllLists();
       }
@@ -109,4 +113,4 @@ document.addEventListener("click", function (e) {
 
 }
 
-autocomplete(document.getElementById("query"), researchServices, bookServices, mangaServices, animeServices);
+autocomplete(document.getElementById("query"), researchServices, bookServices, mangaServices, animeServices, musicServices);
