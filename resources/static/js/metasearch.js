ae1329a3e1153bc3fb8a01fe570a48b9c14a879b
Gumichan01 <luxon.jean.pierre@gmail.com>
2024-10-05 17:35:23 +0200

    Clean and refactorize JS file
    - Remove useless functions
    - Rename variables properly

--- resources/static/js/metasearch.js
+++ resources/static/js/metasearch.js
@@ -7,109 +7,109 @@ var animeServices = ["!anidb", "!anime", "!anilist", "!kitsuanime"]
 var musicServices = ["!discogs", "!freesound", "!musicbrainz", "!opengameart"]
 
 function autocomplete(queryInput, research, books, mangas, animes, music) {
-  /* The autocomplete function takes:
+  /*
+    The autocomplete function takes:
     - the text field element (the input)
     - several array of possible autocompleted values, depending of the domain (research, books, ...)
    */
-  var currentFocus;
-  // execute a function when someone writes in the text field
+  var currentFocus = -1;
+
   queryInput.addEventListener("input", function(e) {
-      var a, b, i, text = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!text) { return false; }
-      currentFocus = -1;
+    var text = this.value;
+    if (!text) {
+      return false;
+    }
 
-      // Select the list of services related to the type of search
-      var tpapers = document.getElementById("type_papers")
-      var tbooks = document.getElementById("type_books")
-      var tmanga = document.getElementById("type_manga")
-      var tanime = document.getElementById("type_anime")
-      var tmusic = document.getElementById("type_music")
-      var completionList;
+    clearAutocompletionList();
+    // Select the list of services related to the type of search
+    var tpapers = document.getElementById("type_papers")
+    var tbooks = document.getElementById("type_books")
+    var tmanga = document.getElementById("type_manga")
+    var tanime = document.getElementById("type_anime")
+    var tmusic = document.getElementById("type_music")
+    var completionList;
 
-      if (tpapers.checked) {
-        completionList = research
-      } else if (tbooks.checked) {
-        completionList = books
-      } else if (tmanga.checked) {
-        completionList = mangas
-      } else if (tanime.checked) {
-        completionList = animes
-      } else if (tmusic.checked) {
-        completionList = music
+    if (tpapers.checked) {
+      completionList = research
+    } else if (tbooks.checked) {
+      completionList = books
+    } else if (tmanga.checked) {
+      completionList = mangas
+    } else if (tanime.checked) {
+      completionList = animes
+    } else if (tmusic.checked) {
+      completionList = music
+    }
+
+    // Display the autocompletion list
+    var autocompletionListElem = document.createElement("DIV");
+    autocompletionListElem.setAttribute("id", this.id + "autocomplete-list");
+    autocompletionListElem.setAttribute("class", "autocomplete-items");
+
+    this.parentNode.appendChild(autocompletionListElem);
+    for (var i = 0; i < completionList.length; i++) {
+      if (completionList[i].substring(0, text.length).toUpperCase() == text.toUpperCase()) {
+        var element = document.createElement("DIV");
+        element.innerHTML = "<strong>" + completionList[i].substring(0, text.length) + "</strong>";
+        element.innerHTML += completionList[i].substring(text.length);
+        element.innerHTML += "<input type='hidden' value='" + completionList[i] + "'>";
+
+        element.addEventListener("click", function(e) {
+          queryInput.value = this.getElementsByTagName("input")[0].value;
+          clearAutocompletionList();
+        });
+
+        autocompletionListElem.appendChild(element);
       }
+    }
+  });
 
-      // Display the autocompletion list
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
+  queryInput.addEventListener("keydown", function(e) {
+      var autocompletionElement = document.getElementById(this.id + "autocomplete-list");
+      if (autocompletionElement) {
+        const DOWN_KEYCODE = 40, UP_KEYCODE = 38, ESCAPE_KEYCODE = 27;
+        var autocompletionList = autocompletionElement.getElementsByTagName("div");
+        if (e.keyCode == DOWN_KEYCODE) {
+          if (currentFocus >= 0) {
+            autocompletionList[currentFocus].classList.remove("autocomplete-active");
+          }
+          currentFocus++;
+          if (currentFocus >= autocompletionList.length) {
+            currentFocus = 0;
+          }
 
-      this.parentNode.appendChild(a);
-      for (i = 0; i < completionList.length; i++) {
-        if (completionList[i].substring(0, text.length).toUpperCase() == text.toUpperCase()) {
-          b = document.createElement("DIV");
-          b.innerHTML = "<strong>" + completionList[i].substr(0, text.length) + "</strong>";
-          b.innerHTML += completionList[i].substr(text.length);
-          b.innerHTML += "<input type='hidden' value='" + completionList[i] + "'>";
+          autocompletionList[currentFocus].classList.add("autocomplete-active");
+          queryInput.value = autocompletionList[currentFocus].getElementsByTagName("input")[0].value + " "
+        } else if (e.keyCode == UP_KEYCODE) {
+          if (currentFocus >= 0) {
+            autocompletionList[currentFocus].classList.remove("autocomplete-active");
+          }
+          currentFocus--;
+          if (currentFocus < 0) {
+            currentFocus = (autocompletionList.length - 1);
+          }
 
-          // I integrate this listener in order to update the selected entry in the autocompletion list
-          b.addEventListener("click", function(e) {
-            queryInput.value = this.getElementsByTagName("input")[0].value;
-            closeAllLists();
-           });
-          a.appendChild(b);
+          autocompletionList[currentFocus].classList.add("autocomplete-active");
+          queryInput.value = autocompletionList[currentFocus].getElementsByTagName("input")[0].value + " "
+        } else if (e.keyCode == ESCAPE_KEYCODE) {
+          clearAutocompletionList();
         }
       }
   });
-  // execute a function presses a key on the keyboard
-  queryInput.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {    // DOWN key pressed
-        currentFocus++;
-        addActive(x);
-        queryInput.value = x[currentFocus].getElementsByTagName("input")[0].value + " "
-      } else if (e.keyCode == 38) { // UP key pressed
-        currentFocus--;
-        addActive(x);
-        queryInput.value = x[currentFocus].getElementsByTagName("input")[0].value + " "
-      } else if (e.keyCode == 27) { // ESCAPE key pressed
-        closeAllLists();
-      }
+
+  function clearAutocompletionList() {
+    var autocompletionElement = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < autocompletionElement.length; i++) {
+      autocompletionElement[i].parentNode.removeChild(autocompletionElement[i]);
+    }
+  }
+
+  document.addEventListener("click", function (e) {
+    var target = e.target;
+    if (target != queryInput) {
+      clearAutocompletionList();
+    }
   });
-
-  function addActive(x) {
-    /*a function to classify an item as "active":*/
-    if (!x) return false;
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    x[currentFocus].classList.add("autocomplete-active");
-  }
-
-  function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-  }
-
-  function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != queryInput) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
-  }
-
-// execute a function when someone clicks in the document:
-document.addEventListener("click", function (e) {
-    closeAllLists(e.target);
-});
 
 }
 
